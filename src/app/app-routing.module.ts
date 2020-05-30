712503ef426ce391141ad975890166c0314b69a0
Yogesh Mishra <yogesh.mishra88@gmail.com>
2020-05-30 22:06:01 +0530

    implemented basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {FirstComponent} from './component/first/first.component';
+import {SecondComponent} from './component/second/second.component';
+import {ThirdComponent} from './component/third/third.component';
+import {PagenotfoundComponent} from './component/pagenotfound/pagenotfound.component';
+
+const routes: Routes = [
+  { path:'first-component',component:FirstComponent},
+  { path:'second-component',component:SecondComponent},
+  { path:'third-component',component:ThirdComponent},
+  { path: '' , redirectTo:'/first-component',pathMatch:'full'},
+  { path:'**',component:PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './component/first/first.component';
+import { SecondComponent } from './component/second/second.component';
+import { ThirdComponent } from './component/third/third.component';
+import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
