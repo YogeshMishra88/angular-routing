541b644d4d1e58b52915dcc2f689ec45821d8ce6
Yogesh Mishra <yogesh.mishra88@gmail.com>
2020-05-31 10:03:16 +0530

    worked on route redirection

--- src/app/component/first/firsta/firsta.component.ts
+++ src/app/component/first/firsta/firsta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-firsta',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
+    console.log("hello fron ngOnInit.....")
+    console.log(id);
   }
 
 }
